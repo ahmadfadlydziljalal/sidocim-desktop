ee583c51b92d6b69c4495198f4ed0f58b34d8083
afdj <ahmadfadlydziljalal@gmail.com>
2023-05-30 21:45:23 +0700

    Initial commit

---
+++ src/main/java/com/tresnamuda/sidocim/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tresnamuda.sidocim;
+
+import com.tresnamuda.sidocim.ui.MainForm;
+
+/**
+ *
+ * @author dzil
+ */
+public class App {
+
+    public static void main(String[] args) {
+        MainForm mf = new MainForm();
+        mf.setVisible(true);
+    }
+}
