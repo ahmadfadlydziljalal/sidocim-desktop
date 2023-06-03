b06bfb125a79b5ffaf697885cb0c0650decbd392
afdj <ahmadfadlydziljalal@gmail.com>
2023-06-03 13:00:37 +0700

    Setidaknya sudah bisa buat PDF per row yang digenerate ke satu directory

--- src/main/java/com/tresnamuda/sidocim/ui/pages/stamp_depo/StampDepoProcessPage.java
+++ src/main/java/com/tresnamuda/sidocim/ui/pages/stamp_depo/StampDepoProcessPage.java
@@ -9,11 +9,19 @@ import com.tresnamuda.sidocim.models.ExcelFileReader;
 import com.tresnamuda.sidocim.utils.CheckboxRenderer;
 import com.tresnamuda.sidocim.utils.XLSFileFilter;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -23,7 +31,18 @@ import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
-
+import org.apache.commons.io.FilenameUtils;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -33,7 +52,7 @@ public class StampDepoProcessPage extends javax.swing.JPanel {
 
     private JTable table;
     private DefaultTableModel tableModel;
-   
+
     /**
      * Creates new form StampDepoProcessPage
      */
@@ -50,11 +69,12 @@ public class StampDepoProcessPage extends javax.swing.JPanel {
         initFileChooser();
     }
 
-    private void initBottomActionPanel(){
-        this.BottomActionPanel.setPreferredSize(new Dimension(App.showWidth(), 50));
-        this.BottomActionPanel.setMaximumSize(new Dimension(App.showWidth(), 50));
+    private void initBottomActionPanel() {
+        this.BottomActionPanel.setPreferredSize(new Dimension(App.showWidth(), 30));
+        this.BottomActionPanel.setMaximumSize(new Dimension(App.showWidth(), 30));
+        this.BottomActionPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
     }
-    
+
     private void initProgressPanel() {
         this.ProgressPanel.setPreferredSize(new Dimension(App.showWidth(), 25));
         this.ProgressPanel.setMaximumSize(new Dimension(App.showWidth(), 25));
@@ -110,8 +130,9 @@ public class StampDepoProcessPage extends javax.swing.JPanel {
 
                 try {
                     tableModel = get();
-                    renderTable();
-                    
+                    repaintTablePanel();
+                    repaintBottomActionPanel();
+
                     progressBar.setValue(100);
                     showNotificationDialog(file.getName() + " berhasil diload ...");
 
@@ -125,22 +146,121 @@ public class StampDepoProcessPage extends javax.swing.JPanel {
         // Start the background task
         worker.execute();
     }
-    
-    private void renderTable() {
-   
+
+    private void repaintTablePanel() {
+
         // Create table
         table = new JTable(tableModel);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        
+
         // Render header checkbox
         TableColumn checkboxColumn = table.getColumnModel().getColumn(0);
         checkboxColumn.setHeaderRenderer(new CheckboxRenderer(table.getTableHeader()));
-        
+
         // Repaint table panel
         TablePanel.removeAll();
         TablePanel.add(new JScrollPane(table));
         TablePanel.repaint();
         TablePanel.revalidate();
+    }
+
+    private void repaintBottomActionPanel() {
+
+        JButton button = new JButton();
+        button.setText("Print to PDF");
+        button.setSize(new Dimension(10, 10));
+        button.addActionListener((ActionEvent event) -> {
+            handleToPrintPdf();
+        });
+
+        BottomActionPanel.removeAll();
+        BottomActionPanel.add(button);
+        BottomActionPanel.repaint();
+        BottomActionPanel.revalidate();
+    }
+
+    private int[] getSelectedRows() {
+        List<Integer> selectedRowsList = new ArrayList<>();
+
+        // Iterate over the table model
+        for (int row = 0; row < tableModel.getRowCount(); row++) {
+            // Check the value of the checkbox column for each row
+            boolean selected = (boolean) tableModel.getValueAt(row, 0);
+            if (selected) {
+                // Add the selected row index to the list
+                selectedRowsList.add(row);
+            }
+        }
+
+        // Convert the list to an array
+        int[] selectedRows = new int[selectedRowsList.size()];
+        for (int i = 0; i < selectedRowsList.size(); i++) {
+            selectedRows[i] = selectedRowsList.get(i);
+        }
+
+        return selectedRows;
+    }
+
+    private void handleToPrintPdf() {
+        // Get the selected rows
+        int[] selectedRows = getSelectedRows();
+
+        // Print the selected rows
+        for (int row : selectedRows) {
+            generatePDF(row);
+        }
+    }
+
+    private void generatePDF(int row) {
+
+        // Generate the PDF file
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage();
+            document.addPage(page);
+
+            PDType1Font font = PDType1Font.HELVETICA_BOLD;
+            PDResources resources = new PDResources();
+            resources.put(COSName.getPDFName("Font"), font);
+            page.setResources(resources);
+
+            String filePath;
+
+            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+
+                contentStream.beginText();
+                contentStream.setFont(font, 12);
+                contentStream.newLineAtOffset(100, 700);
+
+                for (int col = 0; col < tableModel.getColumnCount(); col++) {
+                    Object value = tableModel.getValueAt(row, col);
+
+                    if (value != null) {
+
+                        contentStream.showText(value.toString());
+                    }
+                }
+
+                contentStream.endText();
+            }
+
+            File file = new File(pathFileJTextField.getText());
+            String extractedPath = FilenameUtils.removeExtension(
+                    FilenameUtils.removeExtension(file.getName())
+            );
+
+            String directoryPath = System.getProperty("user.dir") + "/reports/" + extractedPath ;
+            File directory = new File(directoryPath);
+            if (!directory.exists()) {
+                 directory.mkdirs();
+            }
+
+            filePath = directoryPath + "/" + table.getValueAt(row, 2) + ".pdf";
+            document.save(filePath);
+            System.out.println("PDF file generated for row " + row + " at " + filePath);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void showNotificationDialog(String message) {
